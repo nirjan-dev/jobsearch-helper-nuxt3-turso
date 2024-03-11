470a6d5e21dfc6b2d6bbc4a9d1efb01ca32f3a4a
Nirjan Khadka <webdev.nk@gmail.com>
2024-03-11 13:32:14 +0545

    chore: add stylelint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+/* eslint-disable no-unused-expressions */
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module"],
+  modules: ["@nuxtjs/eslint-module", "@nuxtjs/stylelint-module"],
 });
+https;
