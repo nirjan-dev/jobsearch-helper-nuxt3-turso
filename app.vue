5b980ad11f7ae5cf98e565d6958ab541963f37ed
Nirjan Khadka <webdev.nk@gmail.com>
2024-03-13 10:59:19 +0545

    feat: add primevue and tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="card flex justify-center">
+      <h1 class="text-5xl">Test</h1>
+
+      <Button label="Submit" icon="pi pi-check" />
+      <Button label="Success" severity="success" />
+    </div>
+
+    <Card style="width: 25em">
+      <template #header>
+        <img
+          alt="user header"
+          src="https://primefaces.org/cdn/primevue/images/usercard.png"
+        />
+      </template>
+      <template #title> Advanced Card </template>
+      <template #subtitle> Card subtitle </template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </template>
+      <template #footer>
+        <Button icon="pi pi-check" label="Save" />
+        <Button
+          icon="pi pi-times"
+          label="Cancel"
+          severity="secondary"
+          style="margin-left: 0.5em"
+        />
+      </template>
+    </Card>
   </div>
 </template>
+
+<script>
+import "~/assets/css/variables.css";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
+import path from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module", "@nuxtjs/stylelint-module"],
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/stylelint-module",
+    "@nuxtjs/tailwindcss",
+    "nuxt-primevue",
+  ],
+  primevue: {
+    unstyled: true,
+    importPT: { from: path.resolve(__dirname, "./presets/wind/") }, //import and apply preset
+  },
 });
 https;
